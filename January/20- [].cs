b615ca923f4fd347cc6a1f1831c117a6eb6ad86f
Abdelfattah Hezema <abdelfatahhezema@gmail.com>
2022-01-20 19:05:05 +0200

    20th day of Jan - ps: this is fun

---
+++ January/20- [].cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int MinEatingSpeed(int[] piles, int H) {
+        int l = 1, r = 1000000000;
+        while (l < r) {
+            int m = (l + r) / 2, total = 0;
+            foreach (int p in piles)
+                total += (p + m - 1) / m;
+            if (total > H)
+                l = m + 1;
+            else
+                r = m;
+        }
+        return l;
+    }
+}
