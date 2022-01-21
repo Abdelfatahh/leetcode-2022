749b1ba85d51ac4b556337700c7aef13239f31e2
Abdelfattah Hezema <abdelfatahhezema@gmail.com>
2022-01-21 10:34:02 +0200

    21th day of Jan

---
+++ January/21- [134. Gas Station].cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int CanCompleteCircuit(int[] gas, int[] cost) {
+        // greedy algorithm
+        var n = gas.Length;
+        var totalSum = 0;
+        var rangeSum = 0;
+
+        var candidate = 0;
+
+        for (int i = 0; i < n; i++) {
+            var diff = gas[i] - cost[i];
+            totalSum += diff;
+            rangeSum += diff;
+
+            if (rangeSum < 0) {
+                candidate = i + 1;
+                rangeSum = 0;
+            }
+        }
+
+        if (totalSum < 0) return -1;
+
+        return candidate;
+    }
+}
